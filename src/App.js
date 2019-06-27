a72e5768faf6907cc4a0ff03832a350f84cea522
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-06-27 16:30:29 +0200

    Reverted the last change.

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
             <h1>Bookish</h1>
             <div className="books">
                 <div className="book">
-                    <h2 className="title">Refactoring</h2>
+                    <h2 className="title"/>
                 </div>
                 <div className="book">
-                    <h2 className="title">Domain-driven design</h2>
+                    <h2 className="title"/>
                 </div>
             </div>
         </div>
