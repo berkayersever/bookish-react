d73f6deba172b2118d22becdb66e2a861900d61a
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-06-27 16:37:49 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-    return (
-        <div className="App">
-            <h1>Bookish</h1>
-            <div className="books">
-                <div className="book">
-                    <h2 className="title">Refactoring</h2>
-                </div>
-                <div className="book">
-                    <h2 className="title">Domain-driven design</h2>
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>Bookish</h1>
+                <div className="books">
+                    <div className="book">
+                        <h2 className="title">Refactoring</h2>
+                    </div>
+                    <div className="book">
+                        <h2 className="title">Domain-driven design</h2>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
