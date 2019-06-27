2b90e5286b6d6fa0ad4ff4b21d31171689c6aff0
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-06-27 22:37:51 +0200

    Update index.test.js

--- src/components/BookDetail/index.test.js
+++ src/components/BookDetail/index.test.js
@@ -13,6 +13,17 @@ describe('BookDetail', () => {
         const wrapper = shallow(<BookDetail {...props}/>);
         expect(wrapper.find('.description').text()).toEqual("The book about how to do refactoring");
     });
+    it('Shows *more* link when description is too long', () => {
+        const props = {
+            book: {
+                name: "Refactoring",
+                description: "The book about how to do refactoring ...."
+            }
+        };
+        const wrapper = shallow(<BookDetail {...props}/>);
+        expect(wrapper.find('a.show-more').length).toEqual(1);
+        expect(wrapper.find('.description').text()).toEqual("The book about how to do refactoring ....");
+    });
     it('Shows the book name when no description was given', () => {
         const props = {
             book: {
