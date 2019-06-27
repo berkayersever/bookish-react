b5a796be82970ef4352bf87ae5a93064adad4f77
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-06-27 19:45:56 +0200

    Update BookList.js

--- src/BookList.js
+++ src/BookList.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
-function BookList({books}) {
+function BookList({loading, error, books}) {
+    if(loading) { return <div className="loading"/> }
+    if(error)   { return <div className="error"/> }
+
     return (<div className="books">
-            {
-                books.map(book => {
-                    return (<div className="book">
-                        <h2 className="title">{book.name}</h2>
-                    </div>)
-                })
-            }
-        </div>);
+        {
+            books.map(book => {
+                return (<div className="book" key={book.id}>
+                    <h2 className="title">{book.name}</h2>
+                    <a href={`/books/${book.id}`} className="view-detail">View Detail</a>
+                </div>)
+            })
+        }
+    </div>)
 }
 
 export default BookList;
