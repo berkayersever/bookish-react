669bee184badc9c0577de148c94d12a905289f04
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-06-27 22:17:08 +0200

    Update BookListContainer.js

--- src/containers/BookListContainer.js
+++ src/containers/BookListContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import Index from './components/BookList/BookList';
+import Index from '../components/BookList/index';
 
 class BookListContainer extends Component {
     constructor(props) {
