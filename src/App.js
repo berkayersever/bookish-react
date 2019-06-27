ea6200a90daa846101f4f93ca3c45a69245ea49d
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-06-27 17:03:34 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React, { Component } from 'react';
+import BookList from './BookList';
 import './App.css';
 
 class App extends Component {
-    renderBooks(books) {
-        return (
-            <div className="books">
-            {
-                books.map(book => {
-                    return (<div className="book">
-                        <h2 className="title">{book.name}</h2>
-                    </div>)
-                })
-            }
-        </div>
-        );
-    }
-
     render() {
         const books = [{ name: 'Refactoring' }, { name: 'Domain-driven design' }];
         return (
             <div className="App">
                 <h1>Bookish</h1>
-                {this.renderBooks(books)}
+                <BookList books={books}/>
             </div>
         );
     }
