5fb752d52c4998c80d0ef2a51a20bcfe5763d276
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-06-27 22:26:56 +0200

    Update index.test.js

--- src/components/BookDetail/index.js
+++ src/components/BookDetail/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function Index({book}) {
+function BookDetail({book}) {
     return(<div className="detail">
         <h2 className="name">{book.name}</h2>
-        <div className="description">{book.description}</div>
+        <div className="description">{book.description ? book.description : book.name}</div>
     </div>);
 }
 
-export default Index;
+export default BookDetail;
