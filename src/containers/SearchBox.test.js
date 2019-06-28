80a59905906ba1923020036e051cb78d6d5422ca
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-06-28 13:17:16 +0200

    Update SearchBox.test.js

---
+++ src/containers/SearchBox.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import SearchBox from './SearchBox';
+
+describe('SearchBox', () => {
+    it('Handle searching', () => {
+        const onChange = jest.fn();
+        const props = { term: '', onChange };
+        const wrapper = shallow(<SearchBox {...props}/>);
+        expect(wrapper.find('input').length).toEqual(1);
+        wrapper.simulate('change', 'domain');
+        expect(onChange).toHaveBeenCalled();
+        expect(onchange).toHaveBeenCalledWith('domain');
+    })
+});
