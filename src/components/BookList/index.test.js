a3bb5fb34d9c757ffb7206a0943caab810b4a8f8
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-06-28 12:45:15 +0200

    Update index.test.js

--- src/components/BookList/index.test.js
+++ src/components/BookList/index.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import Index from './BookList';
+import BookList from './index';
 
 describe('BookList', () => {
     it('Shows a loading bar when loading', () => {
         const props = {
             loading: true
         };
-        const wrapper = shallow(<Index {...props}/>);
+        const wrapper = shallow(<BookList {...props}/>);
         expect(wrapper.find('.loading').length).toEqual(1);
     });
 
@@ -17,7 +17,7 @@ describe('BookList', () => {
                 "message": "Something went wrong"
             }
         };
-        const wrapper = shallow(<Index {...props}/>);
+        const wrapper = shallow(<BookList {...props}/>);
         expect(wrapper.find('.error').length).toEqual(1);
     });
 
@@ -28,7 +28,7 @@ describe('BookList', () => {
                 {name: "Building Micro-service", id: 2}
             ]
         };
-        const wrapper = shallow(<Index {...props}/>);
+        const wrapper = shallow(<BookList {...props}/>);
         expect(wrapper.find('.book .title').length).toEqual(2);
     })
 });
